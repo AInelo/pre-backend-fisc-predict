129bfe8a7cd6bbcd6d74953e68d184b8691c6f96
Lionel TOTON <totonlionel@gmail.com>
2025-07-20 17:46:59 +0100

    Fix : Cors Origin defined 02 times

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,6 @@ import tpsRoutes from './routes/impots/general/tps/TPS.general.route';
 
 const app = express();
 
-// ✅ Middleware CORS pour autoriser toutes les origines
-app.use(cors({
-  origin: '*', // Autorise tout domaine
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 app.use(bodyParser.json());
 
@@ -20,7 +14,7 @@ app.get('/apiAlive', (_req, res) => {
   res.status(200).json({ message: 'API is alive 🚀' });
 });
 
-app.use('/api/general/reel/', tpsRoutes);
+app.use('/api/general/particulier/', tpsRoutes);
 
 const PORT = 5001;
 app.listen(PORT, () => {
@@ -28,33 +22,3 @@ app.listen(PORT, () => {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-// import express from 'express';
-// import bodyParser from 'body-parser';
-
-// import tpsRoutes from './routes/impots/general/tps/TPS.general.route';
-
-// const app = express();
-// app.use(bodyParser.json());
-
-// // ➕ Route pour vérifier que l'API est vivante
-// app.get('/apiAlive', (_req, res) => {
-//   res.status(200).json({ message: 'API is alive 🚀' });
-// });
-
-// app.use('/api/general/reel/', tpsRoutes);
-
-// const PORT = 5001;
-// app.listen(PORT, () => {
-//   console.log(`Serveur démarré sur http://localhost:${PORT}`);
-// });
